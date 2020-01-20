ea2b150f208d1b24eb5d7b68768005428e64449b
Rebeca Pacheco <rbkgarner@gmail.com>
2020-01-19 21:47:39 -0300

    update, delete e validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -4,24 +4,62 @@ namespace App\Http\Controllers;
 
 use App\Task;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\Validation\Validator;
 
 class TasksController extends Controller
 {
-    public function index() {
+    public function index() 
+    {
 
       return Task::all();
 
     }
 
 
-    public function store(Request $request) {
+    public function store(Request $request) 
+    {
 
+        $request->validate([
+          'name'=>'required|max:64',
+          'complete'=>'required'
+        ]);
         $task = Task::create([
             'name'=>$request->input('name'),
             'complete'=>$request->input('complete')
         ]);
 
         return $task;
+  
+      }
+
+      public function show(Task $task) 
+      {
+
+        return $task;
+  
+      }
+
+      public function update(Request $request, Task $task) 
+      {
+
+        $request->validate([
+          'name'=>'required|max:64'
+        ]);
+
+        $task->name = $request->input('name');
+
+        $task->save();
+
+        return $task;
+  
+      }
+
+      public function erase(Task $task) 
+      {
+
+        $task->delete();
+
+        return response()->json(['sucesso'=>true]);
   
       }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,4 +4,7 @@ use Illuminate\Http\Request;
 
 
 Route::get("tasks", "TasksController@index");
+Route::get("tasks/{task}", "TasksController@show");
 Route::post("tasks", "TasksController@store");
+Route::patch("tasks/{task}", "TasksController@update");
+Route::delete("tasks/{task}", "TasksController@erase");
